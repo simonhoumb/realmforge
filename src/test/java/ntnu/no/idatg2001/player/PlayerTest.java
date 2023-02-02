55d36f991a7361181bea8291936b86a6387d1b95
Eskil Alstad <eskilal@stud.ntnu.no>
2023-02-02 17:30:38 +0100

    Added  Test

---
+++ src/test/java/ntnu/no/idatg2001/player/PlayerTest.java
@@ -0,0 +1,24 @@
+package ntnu.no.idatg2001.player;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PlayerTest {
+
+  @Test
+  void setPlayerName() {
+  }
+
+  @Test
+  void setPlayerHealthPoints() {
+  }
+
+  @Test
+  void setPlayerScore() {
+  }
+
+  @Test
+  void setGold() {
+  }
+}
