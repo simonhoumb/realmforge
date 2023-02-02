dc4b6e00acddeda671fde7fafc8bc7b46447b29f
Simon <106014761+simonhoumb@users.noreply.github.com>
2023-02-02 17:13:02 +0100

    Added classes and methods from the 1. part of the exam

---
+++ src/main/java/ntnu/no/idatg2001/Link.java
@@ -0,0 +1,59 @@
+package ntnu.no.idatg2001;
+
+import java.util.List;
+import java.util.Objects;
+import ntnu.no.idatg2001.actions.Action;
+
+public class Link {
+  private String text;
+  private String reference;
+  private List<Action> actions;
+
+  public Link(String text, String reference) {
+    this.text = text;
+    this.reference = reference;
+  }
+
+  public String getText() {
+    return text;
+  }
+
+  public String getReference() {
+    return reference;
+  }
+
+  public void addAction(Action action) {
+    actions.add(action);
+  }
+
+  public List<Action> getActions() {
+    return actions;
+  }
+
+  @Override
+  public String toString() {
+    return "Link{"
+        + "text='" + text + '\''
+        + ", reference='" + reference + '\''
+        + ", actions=" + actions
+        + '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Link link = (Link) o;
+    return text.equals(link.text) && reference.equals(link.reference) && actions.equals(
+        link.actions);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(text, reference, actions);
+  }
+}
