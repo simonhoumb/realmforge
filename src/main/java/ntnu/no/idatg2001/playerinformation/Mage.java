e0b01d22c10113dcb7d6a797b320b27d29b24afa
Simon <106014761+simonhoumb@users.noreply.github.com>
2023-02-06 16:40:11 +0100

    Merged "Version0.1" onto "simon"

---
+++ src/main/java/ntnu/no/idatg2001/playerinformation/Mage.java
@@ -0,0 +1,16 @@
+package ntnu.no.idatg2001.playerinformation;
+
+public class Mage extends Player {
+  private int mana;
+
+  public Mage(String playerName) {
+
+    super(playerName);
+    this.setPlayerHealthPoints(100);
+    this.mana = 100;
+    this.setPlayerClass(PlayerClass.MAGE);
+    super.addToInventory("Staff");
+    super.addToInventory("Mana Potion");
+    super.addToInventory("Mage Robe");
+  }
+}
