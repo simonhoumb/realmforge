9a765f5ea75dcf8cde5b452c32401bce3f3a7a99
Eskil Alstad <eskilal@stud.ntnu.no>
2023-02-06 14:14:52 +0100

    ReFactor of playerclasses, made to entity for more modular.

--- src/main/java/ntnu/no/idatg2001/Main.java
+++ src/main/java/ntnu/no/idatg2001/Main.java
@@ -1,13 +1,31 @@
 package ntnu.no.idatg2001;
 
 import ntnu.no.idatg2001.playerinformation.Mage;
+import ntnu.no.idatg2001.playerinformation.Player;
+import ntnu.no.idatg2001.playerinformation.PlayerBuilder;
 import ntnu.no.idatg2001.playerinformation.PlayerClass;
 
 public class Main {
 
   public static void main(String[] args) {
-    Mage mage = new Mage("Harry Potter", 150, 0, 0,
-        PlayerClass.MAGE, 150);
+    Player player = new PlayerBuilder()
+        .setPlayerName("Jonas")
+        .setPlayerGold(500)
+        .setPlayerHealthPoints(-10)
+        .setPlayerScore(0)
+        .setPlayerClass(PlayerClass.MAGE)
+        .getPlayer();
+    player.addToInventory("Health Potion");
+    player.addToInventory("Sword");
+    player.addToInventory("Shield");
+
+    System.out.println(player);
+
+    System.out.println(player.getPlayerClass());
+    player.setPlayerClass(PlayerClass.valueOfClassNumber(3));
+    System.out.println(player.getPlayerClass());
+
+   Mage mage = new Mage(20);
     System.out.println(mage);
   }
 }
