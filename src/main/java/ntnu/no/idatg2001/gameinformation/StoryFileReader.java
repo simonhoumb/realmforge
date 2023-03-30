7a5fcd173ec9b3f76cc1e5fbe0b8a2dae6ef6a71
Simon <106014761+simonhoumb@users.noreply.github.com>
2023-03-30 19:58:03 +0200

    Added to StoryFileReader

--- src/main/java/ntnu/no/idatg2001/gameinformation/StoryFileReader.java
+++ src/main/java/ntnu/no/idatg2001/gameinformation/StoryFileReader.java
@@ -1,29 +1,40 @@
 package ntnu.no.idatg2001.gameinformation;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
 
-public class StoryFileReader {
-  public StoryFileReader() {
+public class StoryFileReader{
+  public List<Story> readFile(String fileName) {
+    List<Story> stories = new ArrayList<>();
+    Story story;
+    String storyTitle = "";
+    List<Passage> passagesToAdd = new ArrayList<>();
 
-  }
-  //Bare å slette dette når du har immplimentert den andre FileReaderen om den fungerer
-  //som den skal.
-  public ArrayList<Story> storyArrayList(String filename) throws IOException {
-    ArrayList<Story> story = new ArrayList<>();
-    BufferedReader reader = Files.newBufferedReader(Path.of(filename));
-    String line;
-    while ((line = reader.readLine()) != null ) {
-      System.out.println(line);
+    try (BufferedReader reader = Files.newBufferedReader(Path.of(fileName))) {
+      String currentLine;
+      while ((currentLine = reader.readLine()) != null) {
+        if (storyTitle.equals("")) {
+          storyTitle = currentLine.trim();
+        }
+        if (currentLine.startsWith("::")) {
+          String passageTitle = currentLine.replace(":", "");
+          String passageContent = reader.readLine();
+          passagesToAdd.add(new Passage(passageTitle, passageContent));
+        }
+      }
+        story = new Story(storyTitle, passagesToAdd.get(0));
+      for (Passage passage : passagesToAdd) {
+        story.addPassage(passage);
+        stories.add(story);
+
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
     }
-
-    return storyArrayList(filename);
+    return stories;
   }
-
 }
