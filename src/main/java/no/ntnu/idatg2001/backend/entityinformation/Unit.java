d3ab323889046f2fe84f6bbdc948d7c3e7ec6821
Eskil Alstad <eskilal@stud.ntnu.no>
2023-05-09 22:30:33 +0200

    fixed error in javadoc

--- src/main/java/no/ntnu/idatg2001/backend/entityinformation/Unit.java
+++ src/main/java/no/ntnu/idatg2001/backend/entityinformation/Unit.java
@@ -12,7 +12,7 @@ import java.util.List;
 /**
  * Represents A unit in the game.
 
- * @author Eskil Alstad & Simon Husaas Houmb
+ * @author Eskil Alstad and Simon Husaas Houmb
  * @version 2022-12-07
  */
 @Entity
