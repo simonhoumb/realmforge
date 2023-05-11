f113a9e700700c2abf161894b3970d5c8dc158c1
Simon <106014761+simonhoumb@users.noreply.github.com>
2023-05-11 18:38:00 +0200

    Added to show links as hyperlinks in GameView

--- src/main/java/no/ntnu/idatg2001/frontend/view/GameView.java
+++ src/main/java/no/ntnu/idatg2001/frontend/view/GameView.java
@@ -2,8 +2,7 @@ package no.ntnu.idatg2001.frontend.view;
 
 import java.util.ResourceBundle;
 import javafx.geometry.Pos;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.BorderStroke;
@@ -12,11 +11,12 @@ import javafx.scene.layout.BorderWidths;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 import no.ntnu.idatg2001.backend.SettingsModel;
+import no.ntnu.idatg2001.backend.gameinformation.Link;
 import no.ntnu.idatg2001.backend.gameinformation.Passage;
+import no.ntnu.idatg2001.backend.gameinformation.Story;
 import no.ntnu.idatg2001.frontend.controller.GameController;
 
 public class GameView extends BorderPane {
@@ -24,10 +24,17 @@ public class GameView extends BorderPane {
   private VBox gameVBox;
   private ResourceBundle resourceBundle;
   private GameController controller;
+  private Story story;
 
   public GameView() {
+    //TODO: slett denne test story og passage
+    Passage testPassage1 = new Passage("OpeningPassage", "This is a test passage and the opening passage");
+    Passage testPassage2 = new Passage("Dungeon", "This is the dungeon");
+    testPassage1.addLink(new Link("Go to dungeon", "Dungeon"));
+    this.story = new Story("myStory", testPassage1);
+    this.story.addPassage(testPassage2);
     createLayout();
-    addToGameTextFlow(new Passage("testPassage", "This is a test passage"));
+    addToGameTextFlow(story.getOpeningPassage());
   }
 
   private void createLayout() {
@@ -43,9 +50,18 @@ public class GameView extends BorderPane {
   }
 
   public void addToGameTextFlow(Passage currentPassage) {
-    String passageString = String.format("%s: %s",
+    String passageString = String.format("%s: %s%n",
         currentPassage.getTitle(), currentPassage.getContent());
     gameTextFlow.getChildren().add(new Text(passageString));
+    gameTextFlow.getChildren().add(new Text("Your Choices Are: \n"));
+    for (Link link : currentPassage.getLinks()) {
+      Hyperlink hyperLink = new Hyperlink(String.format("%s ",link.getText()));
+      hyperLink.setOnAction(event -> {
+        clearGameTextFlow();
+        addToGameTextFlow(story.getPassage(link));
+      });
+      gameTextFlow.getChildren().add(hyperLink);
+    }
   }
 
   public void clearGameTextFlow() {
