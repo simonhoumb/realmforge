6cc34675676faafb28f57e2b8400ff7c90598b83
Eskil Alstad <eskilal@stud.ntnu.no>
2023-05-11 19:05:49 +0200

    added a view and controller for edit sotry

---
+++ src/main/java/no/ntnu/idatg2001/frontend/view/EditStoryView.java
@@ -0,0 +1,22 @@
+package no.ntnu.idatg2001.frontend.view;
+
+import javafx.scene.layout.BorderPane;
+import no.ntnu.idatg2001.frontend.controller.EditStoryController;
+
+public class EditStoryView extends BorderPane {
+
+  private EditStoryController controller;
+
+
+  public EditStoryView() {
+
+  }
+
+  public void init() {
+
+  }
+
+  public void setController(EditStoryController controller) {
+    this.controller = controller;
+  }
+}
