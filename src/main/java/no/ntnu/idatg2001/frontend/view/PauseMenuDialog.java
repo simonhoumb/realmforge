1551fae607e02b43c40e51e0ce845c27b3e6b23e
Simon <106014761+simonhoumb@users.noreply.github.com>
2023-05-13 16:25:19 +0200

    Fixed location of resource bundle in PauseMenuDialog

--- src/main/java/no/ntnu/idatg2001/frontend/view/PauseMenuDialog.java
+++ src/main/java/no/ntnu/idatg2001/frontend/view/PauseMenuDialog.java
@@ -2,16 +2,13 @@ package no.ntnu.idatg2001.frontend.view;
 
 import com.jfoenix.controls.JFXButton;
 import java.io.IOException;
-import java.util.Locale;
 import java.util.ResourceBundle;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
@@ -20,9 +17,8 @@ import javafx.stage.StageStyle;
 import no.ntnu.idatg2001.backend.SettingsModel;
 import no.ntnu.idatg2001.frontend.controller.Controller;
 import no.ntnu.idatg2001.frontend.controller.GameController;
-import no.ntnu.idatg2001.frontend.controller.MainMenuController;
 
-public class PauseMenuDialog extends Dialog<ButtonType> implements View {
+public class PauseMenuDialog extends Dialog<ButtonType> {
   private ResourceBundle resourceBundle;
   private GameController controller;
   private JFXButton resumeButton;
@@ -38,7 +34,7 @@ public class PauseMenuDialog extends Dialog<ButtonType> implements View {
     initModality(Modality.APPLICATION_MODAL);
     getDialogPane().getScene().setFill(Color.TRANSPARENT);
     getDialogPane().getStylesheets().add(("css/settingsStyleSheet.css"));
-    this.resourceBundle = ResourceBundle.getBundle("languages/settingsDialog", SettingsModel.getInstance().getLocale());
+    this.resourceBundle = ResourceBundle.getBundle("languages/pauseMenuDialog", SettingsModel.getInstance().getLocale());
     createExitToMenuButton();
     createExitToDesktop();
     createResumeButton();
@@ -68,30 +64,30 @@ public class PauseMenuDialog extends Dialog<ButtonType> implements View {
     }
 
     private void createResumeButton() {
-      resumeButton = new JFXButton("Resume");
+      resumeButton = new JFXButton(resourceBundle.getString("pauseMenu.resume"));
       resumeButton.setCancelButton(true);
       resumeButton.setOnAction(controller::onCloseSource);
     }
 
     private void createSaveButton() {
-      saveButton = new JFXButton("Save");
+      saveButton = new JFXButton(resourceBundle.getString("pauseMenu.save"));
       saveButton.setOnAction(event -> {//TODO add save game dialog
         });
     }
 
     private void createLoadButton() {
-      loadButton = new JFXButton("Load");
+      loadButton = new JFXButton(resourceBundle.getString("pauseMenu.load"));
       loadButton.setOnAction(event -> {//TODO add load game dialog
       });
     }
 
     private void createSettingsButton() {
-      settingsButton = new JFXButton("Settings");
+      settingsButton = new JFXButton(resourceBundle.getString("pauseMenu.settings"));
       settingsButton.setOnAction(event -> controller.onSettingsViewButtonPressed());
     }
 
     private void createExitToMenuButton() {
-      exitToMenuButton = new JFXButton("Exit To Menu");
+      exitToMenuButton = new JFXButton(resourceBundle.getString("pauseMenu.exitToMenu"));
       exitToMenuButton.setOnAction(event -> {
         try {
           controller.onBackToMainMenuButtonPressed(event);
@@ -103,8 +99,8 @@ public class PauseMenuDialog extends Dialog<ButtonType> implements View {
     }
 
     private void createExitToDesktop() {
-      exitToDesktopButton = new JFXButton("Exit To Desktop");
-      exitToDesktopButton.setOnAction(controller::onExitApplication);
+      exitToDesktopButton = new JFXButton(resourceBundle.getString("pauseMenu.exitToDesktop"));
+      exitToDesktopButton.setOnAction(event -> controller.onExitViewButtonPressed(event));
     }
 
     public void update() {
@@ -116,9 +112,4 @@ public class PauseMenuDialog extends Dialog<ButtonType> implements View {
       exitToMenuButton.setText(resourceBundle.getString("pauseMenu.exitToMenu"));
       exitToDesktopButton.setText(resourceBundle.getString("pauseMenu.exitToDesktop"));
     }
-
-  @Override
-  public void setController(Controller controller) {
-    this.controller = (GameController) controller;
-  }
 }
