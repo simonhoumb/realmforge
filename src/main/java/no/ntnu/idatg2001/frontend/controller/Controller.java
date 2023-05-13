64778c349b8c67780ad9bf4c9a88ae6d56b1cde6
Simon <106014761+simonhoumb@users.noreply.github.com>
2023-05-13 16:27:35 +0200

    Removed instance of SettingsDialog in Controller class

--- src/main/java/no/ntnu/idatg2001/frontend/controller/Controller.java
+++ src/main/java/no/ntnu/idatg2001/frontend/controller/Controller.java
@@ -4,14 +4,11 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.stage.Stage;
-import no.ntnu.idatg2001.backend.SettingsModel;
 import no.ntnu.idatg2001.dao.GameDAO;
-import no.ntnu.idatg2001.frontend.view.SettingsDialog;
-import no.ntnu.idatg2001.frontend.view.View;
 
 public abstract class Controller<T> {
-  private View<T> view;
-  SettingsDialog settingsDialog = new SettingsDialog(this);
+
+  protected T view;
 
   public void onCloseSource(ActionEvent event) {
     Node source = (Node) event.getSource();
@@ -19,28 +16,14 @@ public abstract class Controller<T> {
     stage.close();
   }
 
-  public abstract void onSettingsViewButtonPressed();
-
-  public void onSettingSaveButtonPressed(ActionEvent event) {
-    // Update the model with new settings data
-    SettingsModel.getInstance().setLanguageSelection(settingsDialog.getLanguageSelection());
-    SettingsModel.getInstance().setVolumeSliderValue(settingsDialog.getVolumeSliderValue());
-    SettingsModel.getInstance().setMuted(settingsDialog.isMuteCheckBoxSelected());
-
-    // Save the settings data to a file.
-    SettingsModel.getInstance().saveSettings();
-
-    //updates Main Menu.
-    //menuView.updateMainMenu();
-
-    // Close the settings dialog.
-    onCloseSource(event);
-  }
-
   public void onExitApplication(ActionEvent event) {
     event.consume();
     GameDAO.getInstance().close();
     Platform.exit();
     System.exit(0);
+  }
+
+  public void onSettingSaveButtonPressed(ActionEvent event) {
+    //Default implementation
   }
 }
