dc30702309e296b73fa051cd965a0e0c0cd7f11a
Simon <106014761+simonhoumb@users.noreply.github.com>
2023-05-22 04:03:12 +0200

    Renamed Action classes

--- src/main/java/no/ntnu/idatg2001/backend/actions/ItemAction.java
+++ src/main/java/no/ntnu/idatg2001/backend/actions/ItemAction.java
@@ -5,14 +5,14 @@ import jakarta.persistence.Entity;
 import no.ntnu.idatg2001.backend.entityinformation.Unit;
 
 @Entity
-public class InventoryAction extends Action {
+public class ItemAction extends Action {
 
-  public InventoryAction(String item) {
+  public ItemAction(String item) {
     this.value = item;
     this.setActionType(ActionType.ITEM);
   }
 
-  public InventoryAction() {
+  public ItemAction() {
 
   }
 
