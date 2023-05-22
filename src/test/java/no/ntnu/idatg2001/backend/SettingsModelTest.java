39ff59ea63afdea04eb0d8cbe8eca3ff9326eb56
Simon <106014761+simonhoumb@users.noreply.github.com>
2023-05-22 18:30:52 +0200

    Added to SettingsModelTest

---
+++ src/test/java/no/ntnu/idatg2001/backend/SettingsModelTest.java
@@ -0,0 +1,15 @@
+package no.ntnu.idatg2001.backend;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SettingsModelTest {
+  @BeforeEach
+  void setUp() {
+  }
+
+  @AfterEach
+  void tearDown() {
+  }
+}
